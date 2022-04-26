f0f045a714a3fa9148bbb500d2731855f6e5c0c7
Sama-kun <samgar.robot@gmail.com>
2022-04-26 14:06:07 +0600

    First commit

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports={
-    mongoURL:'mongodb+srv://yumeko:Aisha-2003@first.ys95a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    mongoURL:'mongodb+srv://yumeko:your_password@first.ys95a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
     secretOrKey:'ono'
 }
